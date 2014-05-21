d6b3ece4181ad7e3b20836af7aecb96c6db3ec90
stepedons <stepedons@gmail.com>
2014-05-21 06:18:06 -0800

    Modified config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -12,7 +12,6 @@
  * All Rights Reserved.
  * Contributor(s): ______________________________________.
 ********************************************************************************/
-echo "hello";
 include('vtigerversion.php');
 
 // more than 8MB memory needed for graphics
