7f2afb56ea589cdfc0ffbc3c5492b0416fa667ba
constant11 <constant_11@yahoo.com>
2014-05-30 23:54:28 +0800

    Assigned To Customer and Customer Contact Person

---
+++ modules/NextIXfunctions/XCustomers.php
@@ -0,0 +1,38 @@
+<?php
+include_once('include/custom_workflows/XCustomers.php');
+
+$function = $_REQUEST['functionNextIX'];
+$function();	
+
+function activate(){
+	global $current_user;
+
+	$currentModule = 'XCustomers';
+    $record = $_REQUEST['entityid'];
+    
+    $focus = CRMEntity::getInstance($currentModule);
+    $focus->retrieve_entity_info($record, $currentModule);
+    $focus->id  = "x".$record;
+
+	activateCustomers($focus);
+	
+	header("Location: index.php?action=DetailView&module=XCustomers&record=$record");
+}
+
+function deactivate(){
+	global $current_user;
+
+	$currentModule = 'XCustomers';
+    $record = $_REQUEST['entityid'];
+    
+    $focus = CRMEntity::getInstance($currentModule);
+    $focus->retrieve_entity_info($record, $currentModule);
+    $focus->id  = "x".$record;
+
+	deactivateCustomers($focus);
+	
+	header("Location: index.php?action=DetailView&module=XCustomers&record=$record");
+}
+
+
+?>
